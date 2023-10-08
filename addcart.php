01469eefb4f3c04c5d14cea6a742e1b91f222709
shettyshreesha777 <shreeshashetty577@gmail.com>
2023-10-09 00:18:32 +0530

    add to cart completed

--- addcart.php
+++ addcart.php
@@ -1,3 +1,19 @@
 <?php
 include("navbar.php");
+	$conn = mysqli_connect('localhost','root','','ecommerce');
+    $x=$_GET['itemid'];
+    $name=$_SESSION["username"];
+    $query1="SELECT cid
+             FROM customer
+             WHERE cname LIKE '$name'";
+    $result=mysqli_query($conn,$query1);
+    $row=mysqli_fetch_assoc($result);
+    $custid=$row["cid"];
+
+    $insertOrder="INSERT INTO orders (cid,itemid,shipaddress,total)
+                  SELECT cid, itemid, caddress,price
+                  FROM customer, item
+                  WHERE cid='$custid' AND itemid='$x'";
+    $result=mysqli_query($conn,$insertOrder);
+    header('Location: cart.php?cid='.$custid.'');
 ?>
--- cart.php
+++ cart.php
@@ -5,31 +5,56 @@ include("navbar.php");
 <head>
 <style>
 .bb{
-	margin-top: 200px;
-	height:250px;
+	margin: 50px;
+	height:350px;
 	width:800px;
 	border:2px solid rgba(0,0,0,0.5);
+}
+.blocks{
+	display:inline-block;
+	margin:20px;
+}
+.nametext{
+	margin-top:-100px;
+}
+.pricetext{
+	margin-left:400px;
 }
 </style>
 </head>
 <body>
 <h3>Bag</h3>
 <div class="container-fluid">
-	<div class="container bb">
 		  <?php
+		  $count=0;
 		  $conn = mysqli_connect('localhost','root','','ecommerce');
-		  $x=$_GET['itemid'];
+		  $x=$_GET['cid'];
 		  $query="SELECT *
-		          FROM item
-		          WHERE itemid='$x'";
+		          FROM item, orders, customer
+		          WHERE customer.cid='$x' AND orders.itemid=item.itemid";
 			$result=mysqli_query($conn,$query);
-			$row=mysqli_fetch_assoc($result);
-			echo	'<img src='.$row["img1"].'height=200px width=100px >';
+			$rowcount=mysqli_num_rows( $result );
+			echo '<h2 style="margin-top:50px; text-align:center;">'.$_SESSION["username"].'\'s Cart</h2>';
+			echo '<h4 style="text-align:right;">Items in Cart: '.$rowcount.'</h4>';
+			while($row=mysqli_fetch_assoc($result))
+			{
+			$oldprice=$row["price"]+1000;
+			echo '<div class="container bb">';
+			echo "<div class='blocks'>";
+			echo	'<img src='.$row["img1"].'height=400px width=200px >';
+			echo "</div>";
+			echo "<div class='blocks nametext'>";
 			echo '<h3>'.$row["brand"].'</h3>';
 			echo '<h5>'.$row["itemname"].'</h5>';
-			
+			echo "<h5>Size: 44";
+			echo "</div>";
+			echo "<div class='blocks pricetext'>";
+			echo "<h3 style='text-decoration:line-through; color:red; padding-left:130px;'>".$oldprice."</h3>";
+			echo "<h5 style='display:inline-block;'>After Discount: </h5><h3 style='display:inline-block;'>".$row['price']."</h3>";
+			echo "</div>";
+			echo "</div>";				
+			}
 			?>
-	</div>
 </div>
 </body>
 <footer>
